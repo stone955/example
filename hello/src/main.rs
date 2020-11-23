6a47d29291842b3780a0a956a96606befe8f9d52
stone <335612970@qq.com>
2020-11-23 17:09:19 +0800

    stack and heap

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -219,4 +219,24 @@ fn main() {
     }
 
     // stack and heap
+    {
+        struct Point {
+            x: f64,
+            y: f64,
+        }
+
+        fn origin() -> Point {
+            Point {
+                x: 0.0,
+                y: 0.0,
+            }
+        }
+
+        let p1 = origin();
+        let p2 = Box::new(origin());
+        println!("p1 {} bytes", mem::size_of_val(&p1));
+        println!("p2 {} bytes", mem::size_of_val(&p2));
+        let p3 = *p2;
+        println!("p3 {} bytes", mem::size_of_val(&p3));
+    }
 }
