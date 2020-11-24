c79d058603d4faa60f6f0c60231717eb5d5f6047
stone <335612970@qq.com>
2020-11-24 14:48:15 +0800

    enumeration

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -10,6 +10,23 @@ struct Line {
     _end: Point,
 }
 
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Light {
+    Red,
+    Yellow,
+    Green,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Color {
+    // tuple
+    Rgb(u8, u8, u8),
+    // struct
+    Cmy { cyan: u8, magenta: u8, yellow: u8, black: u8 },
+}
+
 fn main() {
     // struct
     {
@@ -30,5 +47,28 @@ fn main() {
             },
         };
         println!("Line start {:?}, end {:?}", line._start, line._end);
+    }
+
+    // enumeration
+    {
+        let light = Light::Red; // Color::Yellow Color::Green
+        let active = match light {
+            Light::Red => "Stop",
+            Light::Yellow => "Ready",
+            Light::Green => "Go"
+        };
+        println!("The light is {:?}, {}!", light, active);
+
+        // let rgb = Color::Rgb(0, 0, 0);
+        // let rgb = Color::Cmy { cyan: 0, magenta: 0, yellow: 0, black: 255 };
+        let rgb = Color::Cmy { cyan: 0, magenta: 0, yellow: 0, black: 0 };
+        match rgb {
+            Color::Rgb(0, 0, 0)
+            | Color::Cmy { cyan: _, magenta: _, yellow: _, black: 255 } => {
+                println!("Color is Black")
+            }
+            Color::Rgb(r, g, b) => println!("Color rgb is {}, {}, {}", r, g, b),
+            _ => println!("Color is what?")
+        };
     }
 }
